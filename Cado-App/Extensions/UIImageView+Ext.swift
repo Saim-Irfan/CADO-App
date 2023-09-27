22f7f879c3d19419d631d35162aeb890728c43a2
Saim-Irfan <saim.irfan@nextgeni.com>
2023-09-27 21:47:26 +0500

    Made an extension for UIImageView that loads an image from a given URL.

---
+++ Cado-App/Extensions/UIImageView+Ext.swift
@@ -0,0 +1,25 @@
+//
+//  UIImage+Ext.swift
+//  Cado-App
+//
+//  Created by Saim on 27/09/2023.
+//
+
+import Foundation
+import UIKit
+
+extension UIImageView {
+    
+    
+    func loadImage(url: URL) {
+        URLSession.shared.dataTask(with: url) { data, response, error in
+            guard let data = data else {
+                return
+            }
+            
+            DispatchQueue.main.async {
+                self.image = UIImage(data: data)!
+            }
+        }.resume()
+    }
+}
