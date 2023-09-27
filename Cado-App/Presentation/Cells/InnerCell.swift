05e975edf435b2c032bfa9332bc9a0771c3dc2ff
Saim-Irfan <saim.irfan@nextgeni.com>
2023-09-27 21:53:37 +0500

    A collection view cell for the collection view that is used to display products in a page controlled.

---
+++ Cado-App/Presentation/Cells/InnerCell.swift
@@ -0,0 +1,16 @@
+//
+//  InnerCell.swift
+//  Cado-App
+//
+//  Created by Saim on 27/09/2023.
+//
+
+import UIKit
+
+class InnerCell: UICollectionViewCell {
+    static let identifier = "innerCell"
+    
+    @IBOutlet var mainImgView: UIImageView!
+    
+    @IBOutlet var titleLbl: UILabel!
+}
