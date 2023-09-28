acf2f2cc7addf56792da26b3e11847aaf79d44d5
Saim-Irfan <saim.irfan@nextgeni.com>
2023-09-28 20:36:33 +0500

    Created helpful methods

--- Cado-App/Managers/CartItemManager.swift
+++ Cado-App/Managers/CartItemManager.swift
@@ -22,11 +22,45 @@ class CartItemManager {
         cartItemRepository.get(byIdentifier: id)
     }
     
+    func getAll(byUserId userId: Int) -> [CartItem] {
+        let cartItems = getAll()
+        
+        return cartItems.filter { cartItem in
+            return cartItem.userId == userId
+        }
+    }
+    
     func update(_ cartItem: CartItem) -> Bool {
         cartItemRepository.update(record: cartItem)
     }
     
     func delete(byId id: Int) -> Bool {
         cartItemRepository.delete(byIdentifier: id)
+    }
+    
+    func returnCartItemId(for userId: Int, productId: Int) -> Int? {
+        let items = getAll(byUserId: userId)
+        
+        guard let itemIndex = items.firstIndex (where: { cartItem in
+            cartItem.productId == productId
+        }) else {
+            return nil
+        }
+        
+        let cartItemId = items[itemIndex].id
+        
+        return cartItemId
+    }
+    
+    func addToCart(userId: Int, productId: Int, quantity: Int) {
+        guard let cartItemId = returnCartItemId(for: userId, productId: productId) else {
+            let newCartItem = CartItem(userId: userId, productId: productId, quantity: quantity)
+            create(newCartItem)
+            return
+        }
+        
+        let cartItem = get(byId: cartItemId)!
+        
+        cartItem.quantity += quantity
     }
 }
