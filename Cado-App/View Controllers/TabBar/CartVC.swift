487464a690b5f82ea90ec2eae9d1b6809149d53e
Saim-Irfan <saim.irfan@nextgeni.com>
2023-09-28 21:26:39 +0500

    Added logic to display cart items in a table view

--- Cado-App/View Controllers/TabBar/CartVC.swift
+++ Cado-App/View Controllers/TabBar/CartVC.swift
@@ -10,17 +10,57 @@ import UIKit
 class CartVC: UIViewController {
     static let storyboardIdentifier = "cartVC"
     
+    @IBOutlet var totalItemLbl: UILabel!
+    @IBOutlet var cartItemTableView: UITableView!
     
-    
+    private let productManager = ProductManager()
     private let cartItemManager = CartItemManager()
+    
+    private var cartItems: [CartItem] = []
     
     override func viewDidAppear(_ animated: Bool) {
         let loggedInUserId = LoggedUser.getLoggedInUserId()!
+        self.cartItems = cartItemManager.getAll(byUserId: loggedInUserId)
         
-        let cartItems = cartItemManager.getAll(byUserId: loggedInUserId)
+        totalItemLbl.text = "\(cartItems.count) items"
+        
+        cartItemTableView.reloadData()
+        setTableViewBorder()
     }
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        cartItemTableView.dataSource    = self
+    }
+    
+    func setTableViewBorder() {
+        if cartItems.isEmpty {
+            cartItemTableView.layer.borderWidth = 0.0
+        }
+        else {
+            cartItemTableView.layer.borderWidth = 0.5
+        }
+    }
+}
+
+extension CartVC: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        cartItems.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: CartItemCell.identifier) as! CartItemCell
+        
+        let cartItem = cartItems[indexPath.row]
+        let product = productManager.get(byId: cartItem.productId)!
+        
+        cell.mainImgView.loadImage(url: URL(string: product.imageUrl)!)
+        cell.nameLbl.text = product.name
+        cell.shortDescriptionLbl.text = product.shortDescription
+        cell.quantityTxtField.text = String(cartItem.quantity)
+        
+        
+        return cell
     }
 }
