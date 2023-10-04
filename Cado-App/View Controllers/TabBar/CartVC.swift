f2080a89d3d157e12628f45cb177884ff50b62ec
Saim-Irfan <saim.irfan@nextgeni.com>
2023-10-04 11:55:40 +0500

    The height of table view will be adjusted based on the cart Item. The checkout button will be disabled if there are no cart items present. Distributed the code into functions. Added code for CartItemCell deleteBtn callback.

--- Cado-App/View Controllers/TabBar/CartVC.swift
+++ Cado-App/View Controllers/TabBar/CartVC.swift
@@ -12,6 +12,9 @@ class CartVC: UIViewController {
     
     @IBOutlet var totalItemLbl: UILabel!
     @IBOutlet var cartItemTableView: UITableView!
+    @IBOutlet var tableViewHeightConstraint: NSLayoutConstraint!
+    
+    @IBOutlet var proceedToCheckoutBtn: PrimaryButton!
     
     @IBOutlet var totalPriceLabel: UILabel!
     
@@ -21,12 +24,54 @@ class CartVC: UIViewController {
     private var cartItems: [CartItem] = []
     
     override func viewDidAppear(_ animated: Bool) {
+        tabBarController?.navigationItem.title = "Cart"
+        
+        configureScreen()
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        cartItemTableView.dataSource    = self
+    }
+    
+    func configureScreen() {
+        configureCart()
+        setTotalPrice()
+                
+        setTableViewDesign()
+        cartItemTableView.reloadData()
+    }
+    
+    func setTableViewDesign() {
+        if cartItems.isEmpty {
+            tableViewHeightConstraint.constant = 0.0
+            cartItemTableView.isHidden = true
+            cartItemTableView.layer.borderWidth = 0.0
+            proceedToCheckoutBtn.setDisabled()
+        }
+        else {
+            if cartItems.count < 2 {
+                tableViewHeightConstraint.constant = 150.0
+            }
+            else {
+                tableViewHeightConstraint.constant = 350.0
+            }
+            
+            cartItemTableView.isHidden = false
+            cartItemTableView.layer.borderWidth = 0.5
+            proceedToCheckoutBtn.setEnabled()
+        }
+    }
+    
+    func configureCart() {
         let loggedInUserId = LoggedUser.getLoggedInUserId()!
         self.cartItems = cartItemManager.getAll(byUserId: loggedInUserId)
-        
+    }
+    
+    func setTotalPrice() {
         totalItemLbl.text = "\(cartItems.count) items"
         
-//        totalPriceLabel.text = "0.00 AED"
         var totalPrice: Double = 0.0
         for cartItem in cartItems {
             let product = productManager.get(byId: cartItem.productId)!
@@ -34,27 +79,6 @@ class CartVC: UIViewController {
         }
         
         totalPriceLabel.text = "\(totalPrice) AED"
-        
-        
-        setTableViewDesign()
-        cartItemTableView.reloadData()
-    }
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        cartItemTableView.dataSource    = self
-    }
-    
-    func setTableViewDesign() {
-        if cartItems.isEmpty {
-            cartItemTableView.isHidden = true
-            cartItemTableView.layer.borderWidth = 0.0
-        }
-        else {
-            cartItemTableView.isHidden = false
-            cartItemTableView.layer.borderWidth = 0.5
-        }
     }
 }
 
@@ -63,16 +87,25 @@ extension CartVC: UITableViewDataSource {
         cartItems.count
     }
     
+    
+    
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: CartItemCell.identifier) as! CartItemCell
         
         let cartItem = cartItems[indexPath.row]
         let product = productManager.get(byId: cartItem.productId)!
         
+        
         cell.mainImgView.loadImage(url: URL(string: product.imageUrl)!)
         cell.nameLbl.text = product.name
         cell.shortDescriptionLbl.text = product.shortDescription
         cell.quantityTxtField.text = String(cartItem.quantity)
+        
+        cell.cartItemId = cartItem.id
+        cell.deleteBtnCallback = { cartItemId in
+            self.cartItemManager.delete(byId: cartItemId)
+            self.configureScreen()
+        }
         
         
         return cell
