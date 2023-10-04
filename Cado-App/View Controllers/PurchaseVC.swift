9bed7f2e8a1941db33bac2308dea45861210ac44
Saim-Irfan <saim.irfan@nextgeni.com>
2023-10-04 17:52:01 +0500

    Added a view controller to display Purchase options. Currently its really basic

---
+++ Cado-App/View Controllers/PurchaseVC.swift
@@ -0,0 +1,34 @@
+//
+//  PurchaseVC.swift
+//  Cado-App
+//
+//  Created by Saim on 04/10/2023.
+//
+
+import UIKit
+
+class PurchaseVC: UIViewController {
+    static let storyboardIdentifier = "purchaseVC"
+
+    override func viewDidAppear(_ animated: Bool) {
+        navigationItem.title = "Purchase"
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
