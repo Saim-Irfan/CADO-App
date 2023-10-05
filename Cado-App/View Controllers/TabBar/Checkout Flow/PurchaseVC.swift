cbf56894f6ef930532ba76f72a036e231ca30081
Saim-Irfan <saim.irfan@nextgeni.com>
2023-10-05 15:11:44 +0500

    When the purchase button is pressed a new order will be generated.

--- Cado-App/View Controllers/TabBar/Checkout Flow/PurchaseVC.swift
+++ Cado-App/View Controllers/TabBar/Checkout Flow/PurchaseVC.swift
@@ -21,6 +21,13 @@ class PurchaseVC: UIViewController {
     
     @IBOutlet var completePurchaseBtn: PrimaryButton!
     
+    private let orderManager = OrderManager()
+    private let orderItemManager = OrderItemManager()
+    
+    private let cartItemManager = CartItemManager()
+    
+    private let productManager = ProductManager()
+    
     var totalPrice = 0.0
     
     override func viewDidAppear(_ animated: Bool) {
@@ -76,5 +83,28 @@ class PurchaseVC: UIViewController {
         paymentRadioOne.setAsDisabled()
         paymentRadioTwo.setAsEnabled()
         completePurchaseBtn.setEnabled()
+    }
+    
+    @IBAction func purchaseBtnTapped(_ sender: PrimaryButton) {
+        let loggedInUserId = LoggedUser.getLoggedInUserId()!
+        
+        let currentDate = Date.now
+        var dateComponent = DateComponents()
+        dateComponent.setValue(3, for: .day)
+        let deliveryDate = Calendar.current.date(byAdding: dateComponent, to: currentDate)!
+        
+        let newOrder = Order(userId: loggedInUserId, date: currentDate, status: .inProgress, totalPrice: totalPrice, deliveryDate: deliveryDate)
+        
+        orderManager.create(newOrder)
+        
+        let cartItemListOfUser = cartItemManager.getAll(byUserId: loggedInUserId)
+        
+        for cartItem in cartItemListOfUser {
+            let product = productManager.get(byId: cartItem.productId)!
+            
+            let newOrderItem = OrderItem(orderId: newOrder.id, productId: cartItem.productId, quantity: cartItem.quantity, price: product.price)
+            
+            orderItemManager.create(newOrderItem)
+        }
     }
 }
