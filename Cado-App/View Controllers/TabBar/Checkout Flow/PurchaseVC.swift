1fd91582b1e33f06bfc73d03e0331f2a2abbdd63
Saim-Irfan <saim.irfan@nextgeni.com>
2023-10-05 14:47:10 +0500

    Completed the design wise functionality.

--- Cado-App/View Controllers/TabBar/Checkout Flow/PurchaseVC.swift
+++ Cado-App/View Controllers/TabBar/Checkout Flow/PurchaseVC.swift
@@ -9,7 +9,20 @@ import UIKit
 
 class PurchaseVC: UIViewController {
     static let storyboardIdentifier = "purchaseVC"
-
+    
+    
+    @IBOutlet var paymentRadioOne: AddressRadioButton!
+    @IBOutlet var paymentRadioTwo: AddressRadioButton!
+    
+    @IBOutlet var orderSummaryStackView: UIStackView!
+    @IBOutlet var orderSummaryDetailHeight: NSLayoutConstraint!
+    
+    @IBOutlet var totalPriceLbl: UILabel!
+    
+    @IBOutlet var completePurchaseBtn: PrimaryButton!
+    
+    var totalPrice = 0.0
+    
     override func viewDidAppear(_ animated: Bool) {
         navigationItem.title = "Purchase"
     }
@@ -17,18 +30,51 @@ class PurchaseVC: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(orderSummaryStackViewTapped))
+        
+        orderSummaryStackView.isUserInteractionEnabled = true
+        orderSummaryStackView.addGestureRecognizer(tapGesture)
+        
+        totalPriceLbl.text = "\(totalPrice) AED"
+        
+        completePurchaseBtn.setDisabled()
+    }
+    
+    @objc func orderSummaryStackViewTapped() {
+        if orderSummaryDetailHeight.constant == 0.0 {
+            orderSummaryDetailHeight.constant = 44.0
+        }
+        else {
+            orderSummaryDetailHeight.constant = 0.0
+        }
+        
+        UIView.animate(withDuration: 0.5, delay: 0.0, options: .beginFromCurrentState, animations: {
+            self.view.layoutIfNeeded()
+        }, completion: nil)
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    
+    @IBAction func giftCardRadioTapped(_ sender: AddressRadioButton) {
+        if sender.isChosen {
+            sender.setAsDisabled()
+        }
+        else {
+            sender.setAsEnabled()
+        }
     }
-    */
-
+    
+    
+    @IBAction func paymentRadioOneTapped(_ sender: AddressRadioButton) {
+        paymentRadioTwo.setAsDisabled()
+        paymentRadioOne.setAsEnabled()
+        completePurchaseBtn.setEnabled()
+    }
+    
+    @IBAction func paymentRadioTwoTapped(_ sender: AddressRadioButton) {
+        paymentRadioOne.setAsDisabled()
+        paymentRadioTwo.setAsEnabled()
+        completePurchaseBtn.setEnabled()
+    }
 }
