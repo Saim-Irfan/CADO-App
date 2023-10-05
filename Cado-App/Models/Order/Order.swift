a691082f0e23bc54ed053678b1aa471d13d98b45
Saim-Irfan <saim.irfan@nextgeni.com>
2023-10-05 15:12:23 +0500

    The order id will automatically increment. Added enum to represent status of the order.

--- Cado-App/Models/Order/Order.swift
+++ Cado-App/Models/Order/Order.swift
@@ -8,19 +8,29 @@
 import Foundation
 
 class Order {
+    private static var availableOrderId = 1
+    
     let id:           Int
     let userId:       Int
     let date:         Date
-    let status:       String
+    let status:       OrderStatus
     let totalPrice:   Double
     let deliveryDate: Date
 
-    init(id: Int, userId: Int, date: Date, status: String, totalPrice: Double, deliveryDate: Date) {
-        self.id           = id
+    init(userId: Int, date: Date, status: OrderStatus, totalPrice: Double, deliveryDate: Date) {
+        self.id           = Order.availableOrderId
         self.userId       = userId
         self.date         = date
         self.status       = status
         self.totalPrice   = totalPrice
         self.deliveryDate = deliveryDate
+        
+        Order.availableOrderId += 1
     }
+}
+
+enum OrderStatus: String {
+    case inProgress     = "In Progress"
+    case hold           = "On Hold"
+    case delivered      = "Delivered"
 }
