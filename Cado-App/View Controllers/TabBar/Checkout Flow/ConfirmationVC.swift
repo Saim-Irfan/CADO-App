263bef2e019c4455a1f95a43b16e83cebb381eda
Saim-Irfan <saim.irfan@nextgeni.com>
2023-10-06 11:03:04 +0500

    Added tappable sections in the label so that the user can go to specified screens after getting success message.

--- Cado-App/View Controllers/TabBar/Checkout Flow/ConfirmationVC.swift
+++ Cado-App/View Controllers/TabBar/Checkout Flow/ConfirmationVC.swift
@@ -9,14 +9,16 @@ import UIKit
 
 class ConfirmationVC: UIViewController {
     static let storyboardIdentifier = "confirmationVC"
-
+    
     @IBOutlet var confirmationLbl: UILabel!
+    @IBOutlet var browseGiftLbl: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         configureNavigationItem()
         configureConfirmationMessage()
+        configureBrowseGiftLabel()
     }
     
     func configureNavigationItem() {
@@ -26,15 +28,44 @@ class ConfirmationVC: UIViewController {
     }
     
     func configureConfirmationMessage() {
-        let text = (confirmationLbl.text)!
-        let underlineAttriString = NSMutableAttributedString(string: text)
-        let textRange = (text as NSString).range(of: "My Orders")
-        underlineAttriString.addAttribute(.underlineStyle, value: 1, range: textRange)
-        
-        confirmationLbl.attributedText = underlineAttriString
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(confirmationMsgTapped(gesture:)))
+        confirmationLbl.addGestureRecognizer(tapGesture)
+        confirmationLbl.isUserInteractionEnabled = true
     }
-
+    
+    func configureBrowseGiftLabel() {
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(removeScreen))
+        browseGiftLbl.addGestureRecognizer(tapGesture)
+        browseGiftLbl.isUserInteractionEnabled = true
+    }
+    
     @objc func removeScreen() {
         navigationController?.popViewController(animated: true)
     }
+    
+    @objc func confirmationMsgTapped(gesture: UITapGestureRecognizer) {
+        let text = (confirmationLbl.text)!
+        let myOrdersTextRange = (text as NSString).range(of: "My Orders")
+        
+        if gesture.didTapAttributedTextInLabel(label: confirmationLbl, inRange: myOrdersTextRange) == false {
+            return
+        }
+        
+        guard var viewControllerList = navigationController?.viewControllers else {
+            return
+        }
+        
+        guard let mainTabBarVC = navigationController?.viewControllers.first(where: { viewController in
+            viewController is UITabBarController
+        }) as? UITabBarController else {
+            return
+        }
+        
+        mainTabBarVC.selectedIndex = CartVC.tabbarIndex
+        viewControllerList.removeLast()
+        
+        navigationController?.setViewControllers([mainTabBarVC], animated: true)
+    }
+    
+    
 }
