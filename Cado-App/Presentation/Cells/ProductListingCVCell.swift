c0f27ab02ac391bea56abe8e631fedbe86e5528a
Saim-Irfan <saim.irfan@nextgeni.com>
2023-10-10 12:54:46 +0500

    Added function that will add shadow in the contentView of the cell

--- Cado-App/Presentation/Cells/ProductListingCVCell.swift
+++ Cado-App/Presentation/Cells/ProductListingCVCell.swift
@@ -15,4 +15,15 @@ class ProductListingCVCell: UICollectionViewCell {
     @IBOutlet var shortDescriptionLbl: UILabel!
     @IBOutlet var priceLbl: UILabel!
     
+    override func awakeFromNib() {
+        setShadow()
+    }
+    
+    func setShadow() {
+        contentView.layer.masksToBounds = false
+        contentView.layer.shadowColor   = UIColor.black.cgColor
+        contentView.layer.shadowOpacity = 0.05
+        contentView.layer.shadowPath    = UIBezierPath(rect: bounds).cgPath
+    }
+    
 }
